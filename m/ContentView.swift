e66b5d42c2b501c61a39c42fb28429ea2ae600c5
zagham arshad <zaghamarshad@yahoo.com>
2024-12-04 17:35:05 -0500

    Initial Commit

---
+++ m/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  m
+//
+//  Created by Zagham Arshad on 2024-12-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ m/mApp.swift
@@ -0,0 +1,17 @@
+//
+//  mApp.swift
+//  m
+//
+//  Created by Zagham Arshad on 2024-12-04.
+//
+
+import SwiftUI
+
+@main
+struct mApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
